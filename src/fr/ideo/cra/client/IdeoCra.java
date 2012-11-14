3a6fee250dfb033abd7371b49d9acd9cac4f5121
Serge Prodhomme <s.prodhomme@ideotechnologies,com>
2012-11-14 12:08:53 +0100

    Commit avant le push

--- src/fr/ideo/cra/client/IdeoCra.java
+++ src/fr/ideo/cra/client/IdeoCra.java
@@ -3,7 +3,7 @@ package fr.ideo.cra.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
-import fr.ideo.cra.client.uibinders.ICMenu;
+import fr.ideo.cra.client.uibinders.layout.general.ICRootView;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -14,9 +14,9 @@ public class IdeoCra implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		
-		ICMenu menu = new ICMenu();
-		RootPanel.get().add(menu);
+
+		ICRootView vue = new ICRootView();
+		RootPanel.get().add(vue);
 	
 	}
 }
